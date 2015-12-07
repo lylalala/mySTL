540b7b2bcd6d561f18668c6ebb4fa3e3d2441392
LiuYang <liuyang070424@163.com>
2015-12-07 13:23:58 +0800

    init

---
+++ mySTL/mySTL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mySTL
+//
+//  Created by ly on 15/12/7.
+//  Copyright © 2015年 ly. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
